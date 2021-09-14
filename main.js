0a1c8ec1a9754bce4bf872da70e178984d9b80c2
Anders Ugland <ugland.anders@gmail.com>
2021-09-15 00:31:42 +0200

    Add random image select functionality

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let bioName = document.querySelector('.name');
 let age = document.querySelector('.age');
 let place = document.querySelector('.place');
 let description__text = document.querySelector('.description__text');
+let imageElement = document.querySelector('.image');
 
 const animal = {
   'name': [
@@ -27,28 +28,43 @@ const animal = {
     'Oslo',
     'Mars',
     'Japan'
-  ]  
+  ],
+
+  'image': [
+    'dog-1',
+    'dog-2',
+    'dog-3',
+    'cat-1',
+    'cat-2',
+    'cat-3',
+  ]
 }
 
-// const button = document.querySelector('.button');
-// button.addEventListener('click', () => { 
-//   createAnimal(animal);
-// });
+const button = document.querySelector('.button');
+button.addEventListener('click', () => { 
+  createAnimal(animal);
+});
 
-// function createAnimal() {
-//   let name = animal.name[Math.floor(Math.random() * animal.name.length)];
-//   greetingName.innerHTML = name;
-//   bioName.innerHTML = 'Name: ' + name;
+function createAnimal() {
+  let name = animal.name[Math.floor(Math.random() * animal.name.length)];
+  greetingName.innerHTML = name;
+  bioName.innerHTML = 'Name: ' + name;
 
-//   age.innerHTML = 'Age: ' + generateAge();
+  age.innerHTML = 'Age: ' + generateAge();
+  pickImage();
   
 
-// }
+}
 
-// function coinFlip() {
-//   return Math.floor(Math.random() * 2);
-// }
+function coinFlip() {
+  return Math.floor(Math.random() * 2);
+}
 
-// function generateAge() {
-//   return Math.floor(Math.random() * 120);
-// }
+function generateAge() {
+  return Math.floor(Math.random() * 120);
+}
+
+function pickImage() {
+  imageElement.src = '/img/' + animal.image[Math.floor(Math.random() * animal.image.length)] + '.JPG';
+
+}
